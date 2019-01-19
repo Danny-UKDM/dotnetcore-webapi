724f78363272f0d4cad4cf1058b02f244b1e1d42
Danny Parker <danny.ukdm@gmail.com>
2019-01-19 11:18:41 +0000

    Add skeleton
    - Include Unit Test proj
    - Include editorconfig

---
+++ WebApi.Tests/Controllers/ValuesController/GivenAGetRequestForAllValues.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using WebApi.Controllers;
+using Xunit;
+
+namespace WebApi.Tests
+{
+    public class GivenAGetRequestForAllValues
+    {
+        [Fact]
+        public void ThenAllValuesAreReturned()
+        {
+            var controller = new ValuesController();
+            var values = controller.Get().Value;
+
+            values.Should().NotBeEmpty()
+                .And.HaveCount(2)
+                .And.ContainInOrder(new string[] { "value1", "value2" })
+                .And.ContainItemsAssignableTo<string>();
+        }
+    }
+}
