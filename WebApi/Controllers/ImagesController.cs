bc4d3c3973d2d0b482d24413f247486f3ab09876
Danny Parker <danny.ukdm@gmail.com>
2019-03-02 20:20:20 +0000

    Implement Post action for Images Controller

--- WebApi/Controllers/ImagesController.cs
+++ WebApi/Controllers/ImagesController.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Data.Writers;
+using WebApi.Models;
 
 namespace WebApi.Controllers
 {
@@ -8,20 +11,38 @@ namespace WebApi.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageWriter _imageWriter;
+
+        public ImagesController(IImageWriter imageWriter)
+        {
+            _imageWriter = imageWriter;
+        }
+
+        //-- GET api/images/{imageId}
+        [HttpGet("{imageId}")]
+        [ProducesResponseType(200, Type = typeof(IFormFile))]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> Get(Guid imageId)
+        {
+            return Ok(imageId);
+            //return file != null
+            //    ? Ok(file)
+            //    : (IActionResult)NotFound();
+
+        }
+
         //-- POST api/images
         [HttpPost]
         [ProducesResponseType(201)]
         [ProducesResponseType(400)]
         public async Task<IActionResult> Post(IFormFile file)
         {
+            var modelResult = await _imageWriter.UploadImage(file);
 
+            if (modelResult.Result == ResultStatus.Failed)
+                return BadRequest();
 
-
-            return new OkObjectResult(file);
+            return CreatedAtAction(nameof(Get), new { imageId = modelResult.ImageId }, modelResult);
         }
     }
 }
-
-//image/png
-//image/gif
-//image/jpeg
