bab1ec57c9e7f20ca51b54455fffe319fa142c06
Danny Parker <danny.ukdm@gmail.com>
2019-03-09 18:11:05 +0000

    Complete S3Initialiser and Test

--- LocalstackInitialiser.Tests/GivenAS3Initialiser.cs
+++ LocalstackInitialiser.Tests/GivenAS3Initialiser.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Amazon.Runtime;
 using Amazon.S3;
+using Amazon.S3.Model;
+using FluentAssertions;
 using Xunit;
 
 namespace LocalstackInitialiser.Tests
 {
     public class GivenAS3Initialiser : IDisposable
     {
-        private string _bucketName;
+        private readonly string _bucketName;
         private readonly S3Initialiser _initialiser;
-        private AmazonS3Client _amazonS3Client;
+        private readonly AmazonS3Config _amazonS3Config;
 
         public GivenAS3Initialiser()
         {
@@ -18,19 +19,25 @@ namespace LocalstackInitialiser.Tests
             _initialiser = new S3Initialiser(_bucketName);
             _initialiser.Init();
 
-            var basicAwsCredentials = new BasicAWSCredentials("localstack", "localstack");
-            _amazonS3Client = new AmazonS3Client(basicAwsCredentials, new AmazonS3Config
+            _amazonS3Config = new AmazonS3Config
             {
                 ServiceURL = "http://localhost:4572",
                 AuthenticationRegion = "eu-west-1",
                 ForcePathStyle = true
-            });
+            };
+            ;
         }
 
         [Fact]
-        public async Task TheTheBucketIsCreated()
+        public async Task ThenTheBucketIsCreated()
         {
-            var getBucketLocationResponse = await _amazonS3Client.GetBucketLocationAsync(_bucketName);
+            ListBucketsResponse listBucketsResponse;
+            using (var amazonS3Client = new AmazonS3Client(_amazonS3Config))
+            {
+                listBucketsResponse = await amazonS3Client.ListBucketsAsync();
+            }
+
+            listBucketsResponse.Buckets.Should().Contain(x => x.BucketName == _bucketName);
         }
 
         public void Dispose()
