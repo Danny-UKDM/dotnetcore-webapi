9baffb90573fc160b6e1c50b9a3fffcf73480a25
Danny Parker <danny.ukdm@gmail.com>
2019-04-21 10:34:07 +0100

    Unit test Videos Controller

---
+++ WebApi.Tests/Helpers/VideoBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WebApi.Models;
+
+namespace WebApi.Tests.Helpers
+{
+    public class VideoBuilder
+    {
+        private readonly Video _video;
+
+        private VideoBuilder(string videoName) =>
+            _video = new Video
+            {
+                VideoId = Guid.NewGuid(),
+                PartnerId = Guid.NewGuid(),
+                VideoName = videoName,
+                Url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            };
+
+        public static VideoBuilder CreateVideo(string videoName) => new VideoBuilder(videoName);
+
+        public VideoBuilder WithId(Guid id)
+        {
+            _video.VideoId = id;
+            return this;
+        }
+
+        public Video Build() => _video;
+    }
+}
