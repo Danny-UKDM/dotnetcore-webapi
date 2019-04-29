2bc32c977e21eb3768eda3bde9f2f48d69795308
Danny Parker <danny.parker@myunidays.com>
2019-04-29 19:22:39 +0100

    DataAnnotation for Guid.Empty

--- WebApi/Models/Video.cs
+++ WebApi/Models/Video.cs
@@ -5,8 +5,8 @@ namespace WebApi.Models
 {
     public class Video
     {
-        [Required] public Guid VideoId { get; set; }
-        [Required] public Guid PartnerId { get; set; }
+        [Required] [NoGuidEmpty] public Guid VideoId { get; set; }
+        [Required] [NoGuidEmpty] public Guid PartnerId { get; set; }
         [Required] [StringLength(100)] public string VideoName { get; set; }
         [Required] [StringLength(50)] public string Url { get; set; }
     }
