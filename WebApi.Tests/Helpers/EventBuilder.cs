12feac8ee17269d576c457afeacd305cbd8e243d
Danny Parker <danny.ukdm@gmail.com>
2019-07-10 19:32:50 +0100

    Add Implementation for EventBuilderV2

--- WebApi.Tests/Helpers/EventBuilder.cs
+++ WebApi.Tests/Helpers/EventBuilder.cs
@@ -57,4 +57,61 @@ namespace WebApi.Tests.Helpers
                 OccursOn = @event.OccursOn,
             };
     }
+
+    public class EventBuilderV2
+    {
+        private static readonly int Ticks = (int)DateTime.UtcNow.Ticks;
+        private readonly Guid _eventId = Guid.NewGuid();
+        private Guid _partnerId = Guid.NewGuid();
+        private readonly string _eventName;
+        private const string AddressLine1 = "Some Address";
+        private const string PostalCode = "NG71FB";
+        private const string City = "Some City";
+        private const string Country = "Some Country";
+        private readonly double _latitude = new Random(Ticks).Next(-90, 90);
+        private readonly double _longitude = new Random(Ticks).Next(-80, 180);
+        private readonly DateTime _occursOn = DateTime.UtcNow.AddDays(new Random(Ticks).Next(365));
+        private readonly DateTime _createdAt = DateTime.UtcNow;
+
+        private EventBuilderV2(string eventName) => _eventName = eventName;
+
+        public static EventBuilderV2 CreateEvent(string eventName) => new EventBuilderV2(eventName);
+
+        public EventBuilderV2 WithPartnerId(Guid partnerId)
+        {
+            _partnerId = partnerId;
+            return this;
+        }
+
+        public (Event Event, EventWriteModel EventWriteModel) Build()
+        {
+            return (
+                Event: new Event
+                {
+                    EventId = _eventId,
+                    PartnerId = _partnerId,
+                    EventName = _eventName,
+                    AddressLine1 = AddressLine1,
+                    PostalCode = PostalCode,
+                    City = City,
+                    Country = Country,
+                    Latitude = _latitude,
+                    Longitude = _longitude,
+                    OccursOn = _occursOn,
+                    CreatedAt = _createdAt
+                },
+                EventWriteModel: new EventWriteModel
+                {
+                    PartnerId = _partnerId,
+                    EventName = _eventName,
+                    AddressLine1 = AddressLine1,
+                    PostalCode = PostalCode,
+                    City = City,
+                    Country = Country,
+                    Latitude = _latitude,
+                    Longitude = _longitude,
+                    OccursOn = _occursOn
+                });
+        }
+    }
 }
